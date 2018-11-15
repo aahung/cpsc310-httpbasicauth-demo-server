9b1e0913a01e7f89f866b43ddd974fdb4cf7ef3c
_sam <3804518+aahung@users.noreply.github.com>
2018-11-14 23:55:20 -0800

    Init commit

---
+++ src/rest/Server.ts
@@ -0,0 +1,128 @@
+/**
+ * Created by rtholmes on 2016-06-19. Modified by sam on 2018-11-14.
+ */
+
+import fs = require("fs");
+import restify = require("restify");
+
+/**
+ * This configures the REST endpoints for the server.
+ */
+export default class Server {
+
+    private port: number;
+    private rest: restify.Server;
+
+    constructor(port: number) {
+        this.port = port;
+    }
+
+    /**
+     * Stops the server. Again returns a promise so we know when the connections have
+     * actually been fully closed and the port has been released.
+     *
+     * @returns {Promise<boolean>}
+     */
+    public stop(): Promise<boolean> {
+        const that = this;
+        return new Promise(function (fulfill) {
+            that.rest.close(function () {
+                fulfill(true);
+            });
+        });
+    }
+
+    /**
+     * Starts the server. Returns a promise with a boolean value. Promises are used
+     * here because starting the server takes some time and we want to know when it
+     * is done (and if it worked).
+     *
+     * @returns {Promise<boolean>}
+     */
+    public start(): Promise<boolean> {
+        const that = this;
+        return new Promise(function (fulfill, reject) {
+            try {
+
+                that.rest = restify.createServer({
+                    name: "insightUBC",
+                });
+                that.rest.use(restify.bodyParser({mapFiles: true, mapParams: true}));
+                that.rest.use(
+                    function crossOrigin(req, res, next) {
+                        res.header("Access-Control-Allow-Origin", "*");
+                        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+                        return next();
+                    });
+
+                // This is an example endpoint that you can invoke by accessing this URL in your browser:
+                // http://localhost:4321/echo/hello
+                that.rest.get("/echo/:msg", Server.echo);
+
+                // NOTE: your endpoints should go here
+
+                // This must be the last endpoint!
+                that.rest.get("/.*", Server.getStatic);
+
+                that.rest.listen(that.port, function () {
+                    fulfill(true);
+                });
+
+                that.rest.on("error", function (err: string) {
+                    // catches errors in restify start; unusual syntax due to internal
+                    // node not using normal exceptions here
+                    reject(err);
+                });
+
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    // The next two methods handle the echo service.
+    // These are almost certainly not the best place to put these, but are here for your reference.
+    // By updating the Server.echo function pointer above, these methods can be easily moved.
+    private static echo(req: restify.Request, res: restify.Response, next: restify.Next) {
+
+        // now let's read HTTP basic auth headers
+        let autoHeader = req.headers['authorization'];
+        if (autoHeader === undefined || autoHeader.split(' ')[0] !== 'Basic') {
+            res.header('WWW-Authenticate', 'Basic realm="Basic Auth!"');
+            res.send(401);
+            return next();
+        }
+
+        let user_pass = Buffer.from(autoHeader.split(' ')[1], 'base64').toString();
+        if (user_pass !== 'cpsc:cpsc') {
+            res.header('WWW-Authenticate', 'Basic realm="Wrong password!"');
+            res.send(401);
+            return next();
+        }
+
+
+
+        try {
+            const response = Server.performEcho(req.params.msg);
+            res.json(200, {result: response});
+        } catch (err) {
+            res.json(400, {error: err});
+        }
+        return next();
+    }
+
+    private static performEcho(msg: string): string {
+        if (typeof msg !== "undefined" && msg !== null) {
+            return `${msg}...${msg}`;
+        } else {
+            return "Message not provided";
+        }
+    }
+
+    private static getStatic(req: restify.Request, res: restify.Response, next: restify.Next) {
+        res.write('getStatic ' + req.url);
+        res.end();
+        return next();
+    }
+
+}
